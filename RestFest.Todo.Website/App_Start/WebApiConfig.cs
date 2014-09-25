5e30a1886800938ef3d8792332afebd2374bcb14
Devin Rader <devin@twilio.com>
2014-09-25 10:21:50 -0400

    Added in Media Type formatter

--- RestFest.Todo.Website/App_Start/WebApiConfig.cs
+++ RestFest.Todo.Website/App_Start/WebApiConfig.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 
+using PointW.MediaTypes.Formatters.Hal;
+
 namespace RestFest.Todo.Website
 {
     public static class WebApiConfig
@@ -19,6 +21,8 @@ namespace RestFest.Todo.Website
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.Add(new HalJsonMediaTypeFormatter());
         }
     }
 }
