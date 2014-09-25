41efae8ff3d714990554ae9e0ed550989e64e165
Michael Ottoson <michael@pointw.com>
2014-09-25 09:50:38 -0400

    Added formatter
    no desc

---
+++ PointW.ResourceModel/Resource.cs
@@ -0,0 +1,12 @@
+﻿namespace PointW.ResourceModel
+{
+    public class Resource : IResource
+    {
+        public LinkCollection Relations { get; set; }
+
+        public Resource()
+        {
+            Relations = new LinkCollection();
+        }
+    }
+}
